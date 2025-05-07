c8c522e1309f9eee27cfdf8732afa92ed2a4726e
Wroud <wrouds@gmail.com>
2025-05-07 18:00:42 +0800

    dbeaver/pro#5312 chore: add dev proxy for complex auth REST endpoint

--- webapp/packages/product-base/src/baseConfigurationPlugin.ts
+++ webapp/packages/product-base/src/baseConfigurationPlugin.ts
@@ -1,6 +1,6 @@
 /*
  * CloudBeaver - Cloud Database Manager
- * Copyright (C) 2020-2024 DBeaver Corp and others
+ * Copyright (C) 2020-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0.
  * you may not use this file except in compliance with the License.
@@ -56,6 +56,11 @@ export function baseConfigurationPlugin(mode: string, packageJson: any): PluginO
             origin: 'http://localhost:8080',
             proxy: {
               '/api': {
+                target: envServer,
+                changeOrigin: true,
+                secure: false,
+              },
+              '/auth-complex': {
                 target: envServer,
                 changeOrigin: true,
                 secure: false,
